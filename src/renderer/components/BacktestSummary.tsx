340ae72fc6b5272671c90f2b2f6ad265bf8011f4
Tinh Ngo <tiengtinh@gmail.com>
2018-09-07 15:21:06 +0700

    slider

--- src/renderer/components/BacktestSummary.tsx
+++ src/renderer/components/BacktestSummary.tsx
@@ -5,16 +5,12 @@ import { HorizontalLayout,
   Separator,
   Spacer,
   View } from "nice-react-layout"
-import { FormGroup, InputGroup, Card, Elevation, NumericInput, HTMLSelect, Dialog, Navbar, NavbarHeading, NavbarGroup, Alignment, Divider, H2, ButtonGroup, Button, HTMLTable, Tooltip, Position } from "@blueprintjs/core";
+import { FormGroup, InputGroup, Card, Elevation, NumericInput, HTMLSelect, Dialog, Navbar, NavbarHeading, NavbarGroup, Alignment, Divider, H2, ButtonGroup, Button, HTMLTable, Tooltip, Position, MultiSlider, HandleInteractionKind, Intent } from "@blueprintjs/core";
 import styled from 'styled-components'
 import { Asset } from "../../common/Asset";
 
 interface IState {
 }
-
-const Clear = styled.div`
-
-`
 
 export default class BacktestSummary extends React.Component<any, IState> {
   constructor(props) {
@@ -32,26 +28,33 @@ export default class BacktestSummary extends React.Component<any, IState> {
         ratio: 0.5,
       },
       {
-        symbol: 'ETH',
+        symbol: 'QTUM',
         ratio: 0.5,
       },
       {
-        symbol: 'ETH',
+        symbol: 'NEO',
         ratio: 0.5,
       },
       {
-        symbol: 'ETH',
+        symbol: 'BNB',
         ratio: 0.5,
       },
       {
-        symbol: 'ETH',
+        symbol: 'SALT',
         ratio: 0.5,
       },
       {
-        symbol: 'ETH',
+        symbol: 'DNT',
         ratio: 0.5,
       },
     ]
+
+    const values = {
+      dangerStart: 12,
+      warningStart: 36,
+      warningEnd: 72,
+      dangerEnd: 90,
+    }
 
     return (
       <Card interactive={false} elevation={Elevation.TWO}>
@@ -59,6 +62,41 @@ export default class BacktestSummary extends React.Component<any, IState> {
         <Divider />
 
         <AssetsPropotion data={data}/>
+
+        <MultiSlider
+          defaultTrackIntent={Intent.SUCCESS}
+          labelStepSize={20}
+          max={100}
+          min={0}
+          // onChange={this.handleChange}
+          showTrackFill={false}
+          stepSize={2}
+        >
+          <MultiSlider.Handle
+            type="start"
+            value={values.dangerStart}
+            intentBefore="danger"
+            interactionKind={HandleInteractionKind.PUSH}
+          />
+          <MultiSlider.Handle
+            type="start"
+            value={values.warningStart}
+            intentBefore="warning"
+            interactionKind={HandleInteractionKind.PUSH}
+          />
+          <MultiSlider.Handle
+            type="end"
+            value={values.warningEnd}
+            intentAfter="warning"
+            interactionKind={HandleInteractionKind.PUSH}
+          />
+          <MultiSlider.Handle
+            type="end"
+            value={values.dangerEnd}
+            intentAfter="danger"
+            interactionKind={HandleInteractionKind.PUSH}
+          />
+        </MultiSlider>
 
         <Report />
       </Card>
@@ -125,7 +163,7 @@ class AssetPropotion extends React.Component<IAssetPropotionProps, {}> {
 
 	render() {
     return (
-      <Tooltip content={ <span className='bp3-text-small'>{ this.props.data.symbol }</span> } position={ Position.TOP }>
+      <Tooltip content={ <span className='bp3-text-small'>{ this.props.data.symbol }</span> } position={ Position.TOP_LEFT }>
         <AssetPropotionWrapper>
             <PropotionText className='bp3-text-large'>{ Math.round(this.props.data.ratio * 100 * 100) / 100 }%</PropotionText>
             <AssetImg src={ `cryptocurrency-icons/svg/color/${ this.props.data.symbol.toLowerCase() }.svg` }/>
